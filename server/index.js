55ca61135cab1434ae0fb5ef25093bf89a0f5050
mikemuriith <mikemuriith@gmail.com>
2022-05-21 11:12:39 +0300

    first commit

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const cors = require('cors');
+const mongoose = require('mongoose')
+
 
 const app = express();
 
@@ -6,3 +9,11 @@ const app = express();
 app.listen(4000, ()=>{
     console.log("server started on port 4000")
 })
+
+app.use(cors({
+    origin: ["http://localhost:3000/"],
+    method: ["GET", "POST"],
+    credentials: true
+}))
+
+app.use(express.json());
